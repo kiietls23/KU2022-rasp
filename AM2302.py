c711061eec271aa264635b810be7b35ac648cfb9
kiietls23 <48422575+kiietls23@users.noreply.github.com>
2022-11-11 22:06:52 +0900

    2022-11-11

---
+++ AM2302.py
@@ -0,0 +1,9 @@
+# sudo pip3 install Adafruit_DHT
+
+import Adafruit_DHT as dht
+import time
+
+while True:
+    humidity, temperature = dht.read_retry(dht.DHT22, 4)
+    print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))   
+	time.sleep(1)
