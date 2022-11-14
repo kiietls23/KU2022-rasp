851d12e2a6536ac072029ca77a8f71549f1af1a6
kiietls23 <48422575+kiietls23@users.noreply.github.com>
2022-11-14 20:54:54 +0900

    Update AM2302.py

--- AM2302.py
+++ AM2302.py
@@ -1,6 +1,9 @@
 import adafruit_dht
+import board
 
-dht_device = adafruit_dht.DHT22(4)
+#dht_device = adafruit_dht.DHT22(4)
+dht = adafruit_dht.DHT22(board.D4)
+
 
 tem = dht_device.temperature
 hum = dht_device.humidity
