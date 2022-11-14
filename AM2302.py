5b96eac0b1f9aade1375d9c3e7ac42dbd25da173
kiietls23 <48422575+kiietls23@users.noreply.github.com>
2022-11-14 17:39:01 +0900

    2022-11-14

--- AM2302.py
+++ AM2302.py
@@ -1,10 +1,10 @@
-# sudo pip3 install Adafruit_DHT
-
-import adafruit_dht
-import time
-
+import Adafruit_DHT as dht
+from time import sleep
+#Set DATA pin
 DHT = 4
 while True:
-	humidity, temperature = dht.read_retry(dht.DHT22, 4)
-	print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-	time.sleep(1)
+    #Read Temp and Hum from DHT22
+    h,t = dht.read_retry(dht.DHT22, DHT)
+    #Print Temperature and Humidity on Shell window
+    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t,h))
+    sleep(1) #Wait 5 seconds and read again
