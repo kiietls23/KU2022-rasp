12f355a3c7c2acb5bfdd7531f0de61b197492b47
kiietls23 <48422575+kiietls23@users.noreply.github.com>
2022-11-14 17:05:36 +0900

    Update AM2302.py

--- AM2302.py
+++ AM2302.py
@@ -4,6 +4,6 @@ import Adafruit_DHT as dht
 import time
 
 while True:
-    humidity, temperature = dht.read_retry(dht.DHT22, 4)
-    print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))   
+	humidity, temperature = dht.read_retry(dht.DHT22, 4)
+	print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
 	time.sleep(1)
