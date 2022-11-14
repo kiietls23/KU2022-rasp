9fa5ced5eee4ee674ac92e83f4f10445d48c0add
kiietls23 <48422575+kiietls23@users.noreply.github.com>
2022-11-14 21:17:58 +0900

    Update retry.py

--- retry.py
+++ retry.py
@@ -1,12 +1,9 @@
-import Adafruit_DHT
+import board
+import adafruit_dht
 
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
+dht = adafruit_dht.DHT22(board.D6)
 
-while True:
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+t = dht.temperature
+h = dht.humidity
 
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-    else:
-        print("Failed to retrieve data from humidity sensor")
+print(t, h)
