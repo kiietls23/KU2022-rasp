fb3454d974e3823d06bc2aa3cb571242d8cf4dcf
kiietls23 <48422575+kiietls23@users.noreply.github.com>
2022-11-14 19:56:33 +0900

    Update AM2302.py

--- AM2302.py
+++ AM2302.py
@@ -1,10 +1,22 @@
-import Adafruit_DHT as dht
-from time import sleep
-#Set DATA pin
+import Adafruit_DHT
 
-while True:
-    #Read Temp and Hum from DHT22
-    h,t = dht.read_retry(dht.DHT22, DHT)
-    #Print Temperature and Humidity on Shell window
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t,h))
-    sleep(1) #Wait 5 seconds and read again
+# Sensor should be set to Adafruit_DHT.DHT11,
+# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+sensor = Adafruit_DHT.DHT22
+
+# Example using a Raspberry Pi with DHT sensor
+# connected to GPIO23.
+pin = 23
+
+# Try to grab a sensor reading.  Use the read_retry method which will retry up
+# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+# Note that sometimes you won't get a reading and
+# the results will be null (because Linux can't
+# guarantee the timing of calls to read the sensor).
+# If this happens try again!
+if humidity is not None and temperature is not None:
+    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+else:
+    print('Failed to get reading. Try again!')
