8fb5e1812508335f41fa054b09f3e09729bfa671
kiietls23 <48422575+kiietls23@users.noreply.github.com>
2022-11-15 11:30:25 +0900

    Update AM2302.py

--- AM2302.py
+++ AM2302.py
@@ -1,8 +1,7 @@
 import adafruit_dht
-import board
 
-#dht_device = adafruit_dht.DHT22(4)
-dht = adafruit_dht.DHT22(board.D4)
+dht_device = adafruit_dht.DHT22(4)
+#dht = adafruit_dht.DHT22(board.D4)
 
 
 tem = dht.temperature
