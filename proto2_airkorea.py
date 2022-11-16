4741262b9bfc25058eac6e14ee3103f0d69d9c5e
kiietls23 <48422575+kiietls23@users.noreply.github.com>
2022-11-16 12:41:53 +0900

    2022-11-16

--- proto2_airkorea.py
+++ proto2_airkorea.py
@@ -12,8 +12,8 @@ import requests
 from bs4 import BeautifulSoup
 import pandas
 M = '&numOfRows=1&pageNo=1&stationName=신흥동&dataTerm=DAILY&ver=1.3'
-key='tsFgvelgFo8g9a12hc4f1YCn9z2S16kGxMe7FbBTAaPyEcR8gI2K8bFpegdO2S4ngadYMTWn64d0MFzYHzH71w%3D%3D'
-url='https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey='+key+M
+key ='tsFgvelgFo8g9a12hc4f1YCn9z2S16kGxMe7FbBTAaPyEcR8gI2K8bFpegdO2S4ngadYMTWn64d0MFzYHzH71w%3D%3D'
+url ='https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey=' + key + M
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
