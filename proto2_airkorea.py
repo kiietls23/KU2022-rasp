929955f2de52ad47055a9712a201ab7e54b52671
kiietls23 <48422575+kiietls23@users.noreply.github.com>
2022-11-16 13:36:43 +0900

    Update proto2_airkorea.py

--- proto2_airkorea.py
+++ proto2_airkorea.py
@@ -11,6 +11,12 @@ import adafruit_dht
 import requests
 from bs4 import BeautifulSoup
 import pandas
+
+# requests CERTIFICATE_VERIFY_FAILED 경고 무시
+session = requests.Session()
+session.verify = False
+session.post(url='https://foo.com', data={'bar':'baz'})
+
 M = '&numOfRows=1&pageNo=1&stationName=신흥동&dataTerm=DAILY&ver=1.3'
 key ='tsFgvelgFo8g9a12hc4f1YCn9z2S16kGxMe7FbBTAaPyEcR8gI2K8bFpegdO2S4ngadYMTWn64d0MFzYHzH71w%3D%3D'
 url ='https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey=' + key + M
