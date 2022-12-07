e07a0f87c7d22820f7922132b6b8c7514ca491d7
kiietls23 <48422575+kiietls23@users.noreply.github.com>
2022-12-07 14:11:25 +0900

    Update DHT22_graph.py

--- DHT22_graph.py
+++ DHT22_graph.py
@@ -10,7 +10,7 @@ fig = plt.figure()
 ax = plt.axes(xlim=(0, 50), ylim=(15, 45))
 line, = ax.plot([], [], lw=1, c='yellow', marker='d', ms=2)
 max_points = 50
-line, = ax.plot(np.arange(max_points), np.ones(max_points, dtype=np.float)*np.nan, lw=1, c='yellow', marker='d', ms=2)
+line, = ax.plot(np.arange(max_points), np.ones(max_points, dtype=np.float64)*np.nan, lw=1, c='yellow', marker='d', ms=2)
 
 def init():
     return line
